752ad9a36e12ade592d537c49b2059983381c314
wissem gharbi <55883112+Xkiller92@users.noreply.github.com>
2022-04-11 13:00:46 +0000

    initializing project template
    as i said

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace FileManager
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
