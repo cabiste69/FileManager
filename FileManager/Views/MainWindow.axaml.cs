ceb0de4c0e517c279395f1bb273116da82f1760c
cabiste <ahmedjj354@gmail.com>
2022-04-20 02:03:41 +0100

    preparing for leon

--- FileManager/Views/MainWindow.axaml.cs
+++ FileManager/Views/MainWindow.axaml.cs
@@ -7,10 +7,7 @@ namespace FileManager.Views
         public MainWindow()
         {
             InitializeComponent();
-            Grid g = this.FindControl<Grid>("g");
-            Button b = new Button();
-            b.Content = "aaaaaa";
-            g.Children.Add(b);
+            
         }
     }
 }
